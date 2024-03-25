9e7edf13f4dfec4c87ae629105bb8841fbdf34ba
InesElMahi <ines.el-mahi@alumni.univ-avignon.fr>
2024-03-25 09:30:09 +0100

    TP4

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactoryImpl.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactoryImpl.java
@@ -1,27 +1,26 @@
 package fr.univavignon.pokedex.api;
 
 /**
- * Fabrique de Pokédex
+ * Fournit une implémentation concrète de l'interface IPokedexFactory.
+ * Cette classe est responsable de la création des instances de IPokedex, en utilisant
+ * les services fournis par IPokemonMetadataProvider pour les métadonnées des Pokémon
+ * et IPokemonFactory pour la création des instances de Pokémon.
  * @author Inès El Mahi
  */
 public class IPokedexFactoryImpl implements IPokedexFactory {
+
     /**
-     * Crée une nouvelle instance de Pokédex en utilisant les fournisseurs de métadonnées et de Pokémon
+     * Crée et retourne une nouvelle instance de IPokedex.
      *
-     * @param metadataProvider Fournisseur de métadonnées que le Pokédex créé utilisera.
-     * @param pokemonFactory   Fabrique de Pokémon que le Pokédex créé utilisera.
-     * @return Instance de Pokédex créée.
-     * @throws NullPointerException si metadataProvider ou pokemonFactory est null.
+     * @param metadataProvider Le fournisseur de métadonnées pour les Pokémon,
+     *                         nécessaire pour accéder aux informations de base des Pokémon.
+     * @param pokemonFactory   La fabrique utilisée pour créer de nouvelles instances de Pokémon.
+     * @return Une instance de IPokedex, configurée avec les fournisseurs de métadonnées et de création de Pokémon spécifiés.
      */
     @Override
-    public IPokedex createPokedex(
-            final IPokemonMetadataProvider metadataProvider,
-            final IPokemonFactory pokemonFactory) throws NullPointerException {
-
-        if (metadataProvider == null || pokemonFactory == null) {
-            throw new NullPointerException("metadataProvider et pokemonFactory ne doivent pas être null");
-        }
+    public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
         return new IPokedexImpl(metadataProvider, pokemonFactory);
     }
-
 }
+
+
