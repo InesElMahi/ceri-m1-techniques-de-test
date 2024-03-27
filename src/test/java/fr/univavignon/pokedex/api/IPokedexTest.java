bee937b6d7493a9db382f869da6199e62669d0c9
InesElMahi <ines.el-mahi@alumni.univ-avignon.fr>
2024-03-27 16:12:55 +0100

    TP4

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -229,15 +229,15 @@ public class IPokedexTest {
     }
 
     @Test
-    public void getPokemonWithInvalidIdThrowsException() {
-        IPokemonMetadataProvider metadataProvider = mock(IPokemonMetadataProvider.class);
-        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
-        Pokedex realPokedex = new Pokedex(metadataProvider, pokemonFactory);
+    public void testGetPokemonWithNegativeIdThrowsException() {
+        IPokemonMetadataProvider mockMetadataProvider = mock(IPokemonMetadataProvider.class);
+        IPokemonFactory mockPokemonFactory = mock(IPokemonFactory.class);
+        Pokedex pokedex = new Pokedex(mockMetadataProvider, mockPokemonFactory);
 
-        assertThrows(PokedexException.class, () -> realPokedex.getPokemon(-1));
-        assertThrows(PokedexException.class, () -> realPokedex.getPokemon(0));
+        int negativeId = -1;
+
+        assertThrows(PokedexException.class, () -> pokedex.getPokemon(negativeId),
+                "Demander un Pokémon avec un ID négatif devrait lever une PokedexException.");
     }
-
-
 
 }
