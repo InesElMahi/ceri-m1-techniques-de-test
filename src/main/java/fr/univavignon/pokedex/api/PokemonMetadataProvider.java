6edd47aadfd160b186e879a11ac259a2c8f89836
InesElMahi <ines.el-mahi@alumni.univ-avignon.fr>
2024-03-27 17:17:07 +0100

    TP4

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -3,35 +3,33 @@ package fr.univavignon.pokedex.api;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class PokemonMetadataProvider implements IPokemonMetadataProvider {
 
     private final Map<Integer, PokemonMetadata> metadataMap;
 
-
     public PokemonMetadataProvider() {
         this.metadataMap = new HashMap<>();
-        initialiserMetadonnees();
     }
-
-    private void initialiserMetadonnees() {
-        // Initialise les métadonnées pour certains Pokémon spécifiques
-        metadataMap.put(1, new PokemonMetadata(1, "Bulbizarre", 45, 49, 49));
-        metadataMap.put(2, new PokemonMetadata(2, "Herbizarre", 60, 62, 63));
-        metadataMap.put(3, new PokemonMetadata(3, "Florizarre", 80, 82, 83));
-        metadataMap.put(133, new PokemonMetadata(133, "Aquali", 186, 168, 260));
-
-        for (int i = 4; i <= 150; i++) {
-            metadataMap.put(i, new PokemonMetadata(i, "Non Capturé", 0, 0, 0));
-        }
-    }
-
 
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        PokemonMetadata metadata = metadataMap.get(index);
-        if (metadata == null) {
+        if (index <= 0 || index > 150) {
             throw new PokedexException("Index invalide : " + index);
         }
+
+        PokemonMetadata metadata = metadataMap.get(index);
+        if (metadata == null) {
+            throw new PokedexException("Métadonnées non trouvées pour l'index : " + index);
+        }
         return metadata;
+    }
+
+
+    public void addOrUpdateMetadata(int index, String name, int attack, int defense, int stamina) {
+        if (index <= 0 || index > 150) {
+            throw new IllegalArgumentException("Index invalide : " + index);
+        }
+        metadataMap.put(index, new PokemonMetadata(index, name, attack, defense, stamina));
     }
 }
