249f9486c866e99904e000a02db6be596ebe8a27
InesElMahi <ines.el-mahi@alumni.univ-avignon.fr>
2024-04-04 22:29:32 +0200

    TP5

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -3,15 +3,30 @@ package fr.univavignon.pokedex.api;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * Fournit des métadonnées pour les Pokémon.
+ * Cette classe permet d'accéder aux métadonnées des Pokémon par leur index.
+ */
 public class PokemonMetadataProvider implements IPokemonMetadataProvider {
 
     private final Map<Integer, PokemonMetadata> metadataMap;
 
+
+    /**
+     * Constructeur de PokemonMetadataProvider.
+     * Initialise un nouveau conteneur pour les métadonnées des Pokémon.
+     */
     public PokemonMetadataProvider() {
         this.metadataMap = new HashMap<>();
     }
 
+    /**
+     * Retourne les métadonnées d'un Pokémon spécifié par son index.
+     *
+     * @param index L'index du Pokémon dans le Pokedex.
+     * @return Les métadonnées du Pokémon correspondant à l'index donné.
+     * @throws PokedexException Si l'index est invalide ou si les métadonnées ne sont pas trouvées.
+     */
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
         if (index <= 0 || index > 150) {
